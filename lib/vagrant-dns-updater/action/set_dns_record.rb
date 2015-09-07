1e5b302566e741b55655fc71d3db559361dc805b
Nassim KACHA <nassim.kacha@blueicefield.com>
2015-09-07 21:14:31 +0200

    add an error handling if the ip could not be retrieved

--- lib/vagrant-dns-updater/action/set_dns_record.rb
+++ lib/vagrant-dns-updater/action/set_dns_record.rb
@@ -15,6 +15,7 @@ module VagrantPlugins
           interface = config.interface
           registrar = Registrar::Ovh.new(config)
           @machine.communicate.execute("ip addr show #{interface} | awk '/inet/ && /#{interface}/{sub(/\\/.*$/,\"\",$2); print $2}'") do |type, output|
+            raise Vagrant::Errors::VagrantError.new, output if type.to_s == 'stderr'
             ip = output
             @machine.ui.info("Pointing #{config.subdomain}.#{config.zone} to #{ip}")
             registrar.set_dns_record(ip)
