7fdd02d37b5086c6c6d64859ffd1e7782d0e7260
Nassim KACHA <nassim.kacha@blueicefield.com>
2015-09-11 15:05:27 +0200

    rename provider config setting by registrar

--- lib/vagrant-dns-updater/config.rb
+++ lib/vagrant-dns-updater/config.rb
@@ -3,7 +3,7 @@ require 'vagrant'
 module VagrantPlugins
   module DnsUpdater
     class Config < Vagrant.plugin(2, :config)
-      attr_accessor :provider
+      attr_accessor :registrar
       attr_accessor :appkey
       attr_accessor :appsecret
       attr_accessor :consumerkey
@@ -14,7 +14,7 @@ module VagrantPlugins
 
 
       def initialize
-        @provider = UNSET_VALUE
+        @registrar = UNSET_VALUE
         @appkey = UNSET_VALUE
         @appsecret = UNSET_VALUE
         @consumerkey = UNSET_VALUE
@@ -25,7 +25,7 @@ module VagrantPlugins
       end
 
       def finalize!
-        @provider = nil if @provider == UNSET_VALUE
+        @registrar = nil if @registrar == UNSET_VALUE
         @appkey = nil if @appkey == UNSET_VALUE
         @appsecret = nil if @appsecret == UNSET_VALUE
         @consumerkey = nil if @consumerkey == UNSET_VALUE
@@ -38,10 +38,10 @@ module VagrantPlugins
       def validate(machine)
         finalize!
         errors = []
-        errors << 'provider parameter is required with a valid value' unless ['ovh'].include?(@provider)
-        errors << 'appkey parameter is required' if @appkey.nil? && @provider == 'ovh'
-        errors << 'appsecret parameter is required' if @appsecret.nil? && @provider == 'ovh'
-        errors << 'consumerkey parameter is required' if @consumerkey.nil? && @provider == 'ovh'
+        errors << 'registrar parameter is required with a valid value' unless ['ovh'].include?(@registrar)
+        errors << 'appkey parameter is required' if @appkey.nil? && @registrar == 'ovh'
+        errors << 'appsecret parameter is required' if @appsecret.nil? && @registrar == 'ovh'
+        errors << 'consumerkey parameter is required' if @consumerkey.nil? && @registrar == 'ovh'
         errors << 'zone parameter is required' if @zone.nil?
         errors << 'subdomain parameter is required' if @subdomain.nil?
         errors << 'interface parameter is required' if @interface.nil?
